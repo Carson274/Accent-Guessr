7b127d1651925e1cbe6a523b1126a1b64b617e29
NathanJohnstonn <132169663+NathanJohnstonn@users.noreply.github.com>
2026-03-02 19:06:22 -0800

    set up basic routing

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export function Home() {
+    return (
+        <>
+            <div className="bg-orange-100 h-screen w-screen">
+                <div className="text-6xl font-bold">AccentGuessr</div>
+            </div>
+        </>
+    )
+}
