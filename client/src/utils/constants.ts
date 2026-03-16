6c73207aabde3ea5cadec28665aff0a8ec8003e3
Carson Secrest <carsonsecrest@gmail.com>
2026-03-15 22:41:01 -0700

    add partykit env variable

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,1 +1,4 @@
-export const BACKEND_URL = 'http://localhost:3000'
+export const BACKEND_URL =
+  import.meta.env.VITE_BACKEND_URL ?? "http://localhost:3000";
+export const PARTYKIT_HOST =
+  import.meta.env.VITE_PARTYKIT_HOST ?? "localhost:1999";
